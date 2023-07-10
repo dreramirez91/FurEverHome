a758134a3acfbe007d9528decf6b13d6d49ebc0c
Dre <dreramirez91@gmail.com>
2023-07-10 15:56:00 -0500

    Chose main font

--- ghi/src/Home.js
+++ ghi/src/Home.js
@@ -3,7 +3,7 @@ function Home() {
     <div className="App">
       <h1 className="display-5 fw-bold">Furever Home</h1>
       <div className="col-lg-5 mx-auto">
-        <p className="lead mb-4">ipsum</p>
+        <p className="lead mb-4">"Connecting Hearts with FurEver Homes"</p>
       </div>
     </div>
   );
