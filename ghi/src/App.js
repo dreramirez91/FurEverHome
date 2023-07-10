c5ec50231b03ec8831f1678b4440c0dbc53ebaf9
Marc <marcreygg@gmail.com>
2023-07-10 14:20:20 -0500

    Authprovider addition

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,21 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 // import { useEffect, useState } from "react";
 import Home from "./Home.js";
 import Signup from "./Signup.js";
+import Login from "./Login.js";
 // import ErrorNotification from "./ErrorNotification";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      {/* <Nav/> */}
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="signup" element={<Signup />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        {/* <Nav/> */}
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="login" element={<Login />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
