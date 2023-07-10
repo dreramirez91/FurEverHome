0b1cadb8e5f4eb8d2d6c898339b16f3a792dbe2e
Dre <dreramirez91@gmail.com>
2023-07-10 16:32:09 -0500

    Bootstrap added to home

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 // import { useEffect, useState } from "react";
 import Home from "./Home.js";
-import Signup from "./Signup.js";
-import Login from "./Login.js";
+// import Signup from "./Signup.js";
+// import Login from "./Login.js";
 // import ErrorNotification from "./ErrorNotification";
 import "./App.css";
 
@@ -15,8 +15,8 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="signup" element={<Signup />} />
-            <Route path="login" element={<Login />} />
+            {/* <Route path="signup" element={<Signup />} /> */}
+            {/* <Route path="login" element={<Login />} /> */}
           </Routes>
         </div>
       </BrowserRouter>
