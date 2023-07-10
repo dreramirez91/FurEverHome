f82edfcc012bb5f45ca093693d8be0b46d3ea365
Marc <marcreygg@gmail.com>
2023-07-10 12:38:22 -0500

    Added boilerplate Home.js

---
+++ ghi/src/Home.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="App">
+      <h1 className="display-5 fw-bold">Furever Home</h1>
+      <div className="col-lg-5 mx-auto">
+        <p className="lead mb-4">ipsum</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
