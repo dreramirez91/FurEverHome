60dd4b8134abd34ef10d23b87a185679f550ddbd
Marc <marcreygg@gmail.com>
2023-07-10 14:03:03 -0500

    Fixed App.js

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import { useEffect, useState } from "react";
 import Home from "./Home.js";
+import Signup from "./Signup.js";
 // import ErrorNotification from "./ErrorNotification";
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="signup" element={<Signup />} />
         </Routes>
       </div>
     </BrowserRouter>
