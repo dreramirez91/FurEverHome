473219750da6655882c20d8fed794be85ae71f70
Dre <dreramirez91@gmail.com>
2023-07-10 16:29:36 -0500

    Video background added

--- ghi/src/Home.js
+++ ghi/src/Home.js
@@ -5,9 +5,11 @@ function Home() {
     <div className="App">
       <div className="overlay div-video"></div>
       <video className="video" src={dogBackground} autoPlay loop muted />
-      <h1 className="display-5 fw-bold">Furever Home</h1>
+      <h1 className="display-1 fw-bold">Furever Home</h1>
       <div className="col-lg-5 mx-auto">
-        <p className="lead mb-4">"Connecting Hearts with FurEver Homes"</p>
+        <p className="display-5 lead mb-4">
+          "Connecting Hearts with FurEver Homes"
+        </p>
       </div>
     </div>
   );
