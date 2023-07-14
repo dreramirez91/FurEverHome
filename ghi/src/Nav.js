b291d0d554952f1668190b460b9dfd7d197d4dc0
Dre <dreramirez91@gmail.com>
2023-07-14 13:01:21 -0500

    Added edit and my dogs to nav

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -1,16 +1,38 @@
 import { NavLink } from "react-router-dom";
 import useToken from "@galvanize-inc/jwtdown-for-react";
 import { useAuthContext } from "@galvanize-inc/jwtdown-for-react";
+import { useState, useEffect } from "react";
 
 function Nav() {
   const { logout } = useToken();
   const { token } = useAuthContext();
+  const { fetchWithCookie } = useToken();
+  const [rehomerId, setRehomerId] = useState(null);
+
   let loggedIn = null;
   if (token) {
     loggedIn = true;
   } else {
     loggedIn = false;
   }
+
+  const fetchData = async () => {
+    try {
+      const response = fetchWithCookie(
+        `${process.env.REACT_APP_API_HOST}/token`
+      );
+      const data = await response;
+      const id = data.account.id;
+      setRehomerId(id);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [token]);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid">
@@ -20,14 +42,10 @@ function Nav() {
         <div className="navbar-collapse">
           <ul className="navbar-nav mb-2 mb-lg-0 nav-fill w-100">
             <div>
-              <NavLink
-                className="btn"
-                style={{ color: "white" }}
-                to="/dogs"
-                >
-                  Available Dogs
+              <NavLink className="btn" style={{ color: "white" }} to="/dogs">
+                Available Dogs
               </NavLink>
-              {loggedIn || (
+              {!loggedIn && (
                 <>
                   <NavLink
                     className="btn"
@@ -46,13 +64,29 @@ function Nav() {
                 </>
               )}
               {loggedIn && (
-                <button
-                  onClick={logout}
-                  className="btn"
-                  style={{ color: "white" }}
-                >
-                  Logout
-                </button>
+                <>
+                  <NavLink
+                    className="btn"
+                    style={{ color: "white" }}
+                    to={`/dogs/${rehomerId}/mydogs`}
+                  >
+                    My Dogs
+                  </NavLink>
+                  <NavLink
+                    className="btn"
+                    style={{ color: "white" }}
+                    to={`/dogs/${rehomerId}/create`}
+                  >
+                    Add a Dog
+                  </NavLink>
+                  <button
+                    onClick={logout}
+                    className="btn"
+                    style={{ color: "white" }}
+                  >
+                    Logout
+                  </button>
+                </>
               )}
             </div>
           </ul>
