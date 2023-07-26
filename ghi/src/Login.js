5fc22e98f9eb7e7b6485bf45b333ea95e1a29efb
Dre <dreramirez91@gmail.com>
2023-07-26 17:57:35 -0500

    Bootstrap method failing

--- ghi/src/Login.js
+++ ghi/src/Login.js
@@ -47,40 +47,71 @@ export default function LoginForm() {
 
   return (
     <>
-      <div className="row">
-        <div className="offset-3 col-6">
-          <div className="shadow p-4 mt-4">
-            <h1>Login</h1>
-            <form onSubmit={(e) => handleSubmit(e)} id="account-login">
-              <div className="form-floating mb-3">
-                <input
-                  onChange={handleUsernameChange}
-                  value={username}
-                  placeholder="username"
-                  required
-                  type="text"
-                  id="username"
-                  name="username"
-                  className="form-control"
-                />
-                <label htmlFor="username">Username</label>
-                {loginFailed && <span>Incorrect login information</span>}
+      <div className="modal" tabIndex="-1">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Modal title</h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <div className="row">
+                <div className="offset-3 col-6">
+                  <div className="shadow p-4 mt-4">
+                    <h1>Login</h1>
+                    <form onSubmit={(e) => handleSubmit(e)} id="account-login">
+                      <div className="form-floating mb-3">
+                        <input
+                          onChange={handleUsernameChange}
+                          value={username}
+                          placeholder="username"
+                          required
+                          type="text"
+                          id="username"
+                          name="username"
+                          className="form-control"
+                        />
+                        <label htmlFor="username">Username</label>
+                        {loginFailed && (
+                          <span>Incorrect login information</span>
+                        )}
+                      </div>
+                      <div className="form-floating mb-3">
+                        <input
+                          onChange={handlePasswordChange}
+                          value={password}
+                          placeholder="password"
+                          required
+                          type="password"
+                          id="password"
+                          name="password"
+                          className="form-control"
+                        />
+                        <label htmlFor="password">Password</label>
+                      </div>
+                      <button className="btn btn-primary">Login</button>
+                    </form>
+                  </div>
+                </div>
               </div>
-              <div className="form-floating mb-3">
-                <input
-                  onChange={handlePasswordChange}
-                  value={password}
-                  placeholder="password"
-                  required
-                  type="password"
-                  id="password"
-                  name="password"
-                  className="form-control"
-                />
-                <label htmlFor="password">Password</label>
-              </div>
-              <button className="btn btn-primary">Login</button>
-            </form>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
           </div>
         </div>
       </div>
