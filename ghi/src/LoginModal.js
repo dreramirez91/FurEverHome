90cc347283967ad3e34e4fc9938d0093b9999aae
Dre <dreramirez91@gmail.com>
2023-07-27 09:38:46 -0500

    Almost there

--- ghi/src/LoginModal.js
+++ ghi/src/LoginModal.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import useToken from "@galvanize-inc/jwtdown-for-react";
 import { useAuthContext } from "@galvanize-inc/jwtdown-for-react";
 import { useEffect } from "react";
+import $ from "jquery";
 
 export default function LoginModal(props) {
   console.log(props);
@@ -42,74 +43,91 @@ export default function LoginModal(props) {
     }
   }, [token]);
   return (
-    <div>
-      <div className="modal" id="myModal">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h4 className="modal-title">Login</h4>
-              <button
-                type="button"
-                className="close"
-                onClick={() => props.setShowModal(false)}
-              >
-                &times;
-              </button>
-            </div>
-            <div className="modal-body">
-              <div className="row">
-                <div className="offset-3 col-6">
-                  <div className="shadow p-4 mt-4">
-                    {/* <h1>Login</h1> */}
-                    <form onSubmit={(e) => handleSubmit(e)} id="account-login">
-                      <div className="form-floating mb-3">
-                        <input
-                          onChange={handleUsernameChange}
-                          value={username}
-                          placeholder="username"
-                          required
-                          type="text"
-                          id="username"
-                          name="username"
-                          className="form-control"
-                        />
-                        <label htmlFor="username">Username</label>
-                        {loginFailed && (
-                          <span>Incorrect login information</span>
-                        )}
-                      </div>
-                      <div className="form-floating mb-3">
-                        <input
-                          onChange={handlePasswordChange}
-                          value={password}
-                          placeholder="password"
-                          required
-                          type="password"
-                          id="password"
-                          name="password"
-                          className="form-control"
-                        />
-                        <label htmlFor="password">Password</label>
-                      </div>
-                      <button className="btn btn-primary">Login</button>
-                    </form>
+    <>
+      <script src="path/to/jquery.min.js"></script>
+      <div onClick={(e) => e.stopPropagation()}>
+        <div className="modal" id="myModal">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h4 className="modal-title">Login</h4>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  onClick={() => {
+                    props.setShowModal(false);
+                  }}
+                >
+                  &times;
+                </button>
+              </div>
+              <div className="modal-body">
+                <div className="row">
+                  <div className="offset-3 col-6">
+                    <div className="shadow p-4 mt-4">
+                      {/* <h1>Login</h1> */}
+                      <form
+                        onSubmit={(e) => handleSubmit(e)}
+                        id="account-login"
+                      >
+                        <div className="form-floating mb-3">
+                          <input
+                            onChange={handleUsernameChange}
+                            value={username}
+                            placeholder="username"
+                            required
+                            type="text"
+                            id="username"
+                            name="username"
+                            className="form-control"
+                          />
+                          <label htmlFor="username">Username</label>
+                          {loginFailed && (
+                            <span>Incorrect login information</span>
+                          )}
+                        </div>
+                        <div className="form-floating mb-3">
+                          <input
+                            onChange={handlePasswordChange}
+                            value={password}
+                            placeholder="password"
+                            required
+                            type="password"
+                            id="password"
+                            name="password"
+                            className="form-control"
+                          />
+                          <label htmlFor="password">Password</label>
+                        </div>
+                        <button
+                          className="btn btn-primary"
+                          // {...(loginFailed ? "" :   `${data-dismiss="modal"})}
+                        >
+                          Login
+                        </button>
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-danger"
-                onClick={() => props.setShowModal(false)}
-              >
-                Close
-              </button>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  data-dismiss="modal"
+                  onClick={() => {
+                    props.setShowModal(false);
+                  }}
+                >
+                  Close
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
