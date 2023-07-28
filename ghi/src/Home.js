8ba0274c7c13af32af06d91cbe654659d192c8f3
Dre <dreramirez91@gmail.com>
2023-07-28 12:11:01 -0500

    Simple carousel complete

--- ghi/src/Home.js
+++ ghi/src/Home.js
@@ -1,8 +1,28 @@
 import dogBackground from "./assets/dogBackground.mp4";
 import { useState, useEffect } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
 
 function Home() {
   const [dogs, setDogs] = useState([]);
+  const [fetchSuccessful, setFetchSuccessful] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? dogs.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === dogs.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (dogIndex) => {
+    setCurrentIndex(dogIndex);
+  };
+
   const fetchDogs = async () => {
     const url = `${process.env.REACT_APP_API_HOST}/dogs/`;
     const fetchConfig = {
@@ -18,7 +38,8 @@ function Home() {
       if (response.ok) {
         const data = await response.json();
         setDogs(data.dogs);
-        console.log(dogs);
+        setFetchSuccessful(true);
+        console.log("DOGS --->", dogs);
       }
     } catch (e) {
       console.error(e);
@@ -35,9 +56,9 @@ function Home() {
 
   return (
     <>
+      <div className="overlay div-video"></div>
+      <video className="video" src={dogBackground} autoPlay loop muted />
       <div className="App home-style">
-        <div className="overlay div-video"></div>
-        <video className="video" src={dogBackground} autoPlay loop muted />
         <h1
           className="display-1 fw-bold"
           style={{ textShadow: "4px 4px #1c8cff" }}
@@ -50,49 +71,38 @@ function Home() {
           </p>
         </div>
       </div>
-      <div id="demo" className="carousel slide" data-ride="carousel">
-        <div className="carousel-inner" style={{ marginTop: "10em" }}>
-          <div
-            className="carousel-item active"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignContent: "center",
-            }}
-          >
-            <img
-              src="https://www.mirausa.org/wp-content/uploads/2013/11/our-dogs-gallery-icon.jpg"
-              alt="Our dogs"
-              style={{ maxHeight: "300px", maxWidth: "300px" }}
-            />
-          </div>
-          {dogs?.map((dog) => {
-            return (
-              <div
-                className="carousel-item"
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignContent: "center",
-                }}
-              >
-                <img
-                  src={dog.picture_url}
-                  alt={dog.name}
-                  style={{
-                    maxHeight: "300px",
-                  }}
-                />
-              </div>
-            );
-          })}
+      <div className="max-w-[1400px] h-[780px] w-full py-5 px-5 relative group">
+        <div>
+          <h2 className="home-caption available-dogs">
+            Scroll through our adoptable dogs! See "Available Dogs" for more
+            info!
+          </h2>
         </div>
-        <a className="carousel-control-prev" to="#demo" data-slide="prev">
-          <span className="carousel-control-prev-icon"></span>
-        </a>
-        <a className="carousel-control-next" href="#demo" data-slide="next">
-          <span className="carousel-control-next-icon"></span>
-        </a>
+        {fetchSuccessful && (
+          <div
+            style={{
+              backgroundImage: `url(${dogs[currentIndex].picture_url})`,
+            }}
+            className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+          ></div>
+        )}
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        </div>
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <BsChevronCompactRight onClick={nextSlide} size={30} />
+        </div>
+        <div className="flex top-4 justify-center py-2">
+          {dogs?.map((dog, dogIndex) => (
+            <div
+              key={dogIndex}
+              onClick={() => goToSlide(dogIndex)}
+              className="text-2xl cursor-pointer"
+            >
+              <RxDotFilled />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
