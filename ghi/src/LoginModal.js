1ba5063493439b7a5da553874126ef5aca491c1e
Dre <dreramirez91@gmail.com>
2023-08-04 18:34:44 -0500

    Success

--- ghi/src/LoginModal.js
+++ ghi/src/LoginModal.js
@@ -63,8 +63,8 @@ export default function LoginModal(props) {
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Modal Title</h3>
+                <div className="flex items-start justify-between p-4 mb-3 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">Login</h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => props.setShowModal(false)}
@@ -74,29 +74,58 @@ export default function LoginModal(props) {
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you``
-                    won’t do anything. I was taught I could do everything.
-                  </p>
+                  <form onSubmit={(e) => handleSubmit(e)} id="account-login">
+                    <div className="form-floating mb-3">
+                      <input
+                        onChange={handleUsernameChange}
+                        value={username}
+                        placeholder="username"
+                        required
+                        type="text"
+                        id="username"
+                        name="username"
+                        className="form-control"
+                      />
+                      <label htmlFor="username"></label>
+                      {loginFailed && (
+                        <>
+                          <br />
+                          <div className="alert alert-danger">
+                            Incorrect login information
+                          </div>
+                        </>
+                      )}
+                    </div>
+                    <div className="form-floating">
+                      <input
+                        onChange={handlePasswordChange}
+                        value={password}
+                        placeholder="password"
+                        required
+                        type="password"
+                        id="password"
+                        name="password"
+                        className="form-control"
+                      />
+                      <label htmlFor="password"></label>
+                    </div>
+                  </form>
                 </div>
                 {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                <div className="flex items-center justify-end p-4 border-t border-solid border-slate-200 rounded-b">
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => props.setShowModal(false)}
                   >
-                    Close
+                    Cancel
                   </button>
                   <button
                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => props.setShowModal(false)}
+                    onClick={handleSubmit}
                   >
-                    Save Changes
+                    Login
                   </button>
                 </div>
               </div>
